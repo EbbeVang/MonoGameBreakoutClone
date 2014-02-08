44b12aa8ff34ba6ba66bfacdc558b56b0a87b42a
Ebbe Vang <ev@evang.dk>
2014-02-08 20:27:57 +0100

    added project to GIT

---
+++ GamePage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using MonoGame.Framework;
+
+
+namespace MonoGameBreakoutClone
+{
+    /// <summary>
+    /// The root page used to display the game.
+    /// </summary>
+    public sealed partial class GamePage : SwapChainBackgroundPanel
+    {
+        readonly BreakoutClone _game;
+
+        public GamePage(string launchArguments)
+        {
+            this.InitializeComponent();
+
+            // Create the game.
+            _game = XamlGame<BreakoutClone>.Create(launchArguments, Window.Current.CoreWindow, this);
+        }
+    }
+}
